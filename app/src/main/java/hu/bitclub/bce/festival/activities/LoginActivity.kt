0f5726468b01ddd2a27903bc4b272e9a33560192
EmmChriss <emmchris@protonmail.com>
2021-11-26 21:21:00 +0100

    login with examples

--- app/src/main/java/hu/bitclub/bce/festival/activities/LoginActivity.kt
+++ app/src/main/java/hu/bitclub/bce/festival/activities/LoginActivity.kt
@@ -2,9 +2,7 @@ package hu.bitclub.bce.festival.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import hu.bitclub.bce.festival.R
 
@@ -13,10 +11,19 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        register("asdasd@test.com", "123456")
+        login("asdasd@test.com", "123456")
+    }
+
+    fun register(email: String, password: String) {
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { Toast.makeText(this, "register sikeres", Toast.LENGTH_SHORT).show() }
     }
 
     fun login(email: String, password: String) {
         FirebaseAuth.getInstance()
             .signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { Toast.makeText(this, "login sikeres", Toast.LENGTH_SHORT).show() }
     }
 }
