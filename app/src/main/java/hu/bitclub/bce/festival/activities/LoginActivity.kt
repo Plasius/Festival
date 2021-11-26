ca4467c1ceecc6d88533a51b181d7b5dd546687b
Plasius <Plasius@users.noreply.github.com>
2021-11-26 22:34:07 +0100

    Described login/register todos

--- app/src/main/java/hu/bitclub/bce/festival/activities/LoginActivity.kt
+++ app/src/main/java/hu/bitclub/bce/festival/activities/LoginActivity.kt
@@ -7,19 +7,17 @@ import com.google.firebase.auth.FirebaseAuth
 import hu.bitclub.bce.festival.R
 
 class LoginActivity : AppCompatActivity() {
+    //A login függvény kér egy email-t és egy passwordot, amit az activity_login-ban létrehozott UI elementekből kellene megkapjon
+    //a UI-t úgy kell felépíteni, ahogy a design kéri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        register("asdasd@test.com", "123456")
+
         login("asdasd@test.com", "123456")
     }
 
-    fun register(email: String, password: String) {
-        FirebaseAuth.getInstance()
-            .createUserWithEmailAndPassword(email, password)
-            .addOnSuccessListener { Toast.makeText(this, "register sikeres", Toast.LENGTH_SHORT).show() }
-    }
+
 
     fun login(email: String, password: String) {
         FirebaseAuth.getInstance()
--- app/src/main/java/hu/bitclub/bce/festival/activities/RegisterActivity.kt
+++ app/src/main/java/hu/bitclub/bce/festival/activities/RegisterActivity.kt
@@ -2,11 +2,24 @@ package hu.bitclub.bce.festival.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import hu.bitclub.bce.festival.R
 
 class RegisterActivity : AppCompatActivity() {
+    //A register függvény kér egy email-t és egy passwordot, amit az activity_register-ben létrehozott UI elementekből kellene megkapjon
+    //a UI-t úgy kell felépíteni, ahogy a design kéri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        register("asdasd@test.com", "123456")
+    }
+
+    fun register(email: String, password: String) {
+        FirebaseAuth.getInstance()
+            .createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener { Toast.makeText(this, "register sikeres", Toast.LENGTH_SHORT).show() }
     }
 }
