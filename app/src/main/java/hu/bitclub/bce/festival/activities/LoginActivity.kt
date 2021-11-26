ba6310a2e68610bd6df384c03fc20de6c84e5e52
EmmChriss <emmchris@protonmail.com>
2021-11-26 21:04:39 +0100

    refactor login

--- app/src/main/java/hu/bitclub/bce/festival/activities/LoginActivity.kt
+++ app/src/main/java/hu/bitclub/bce/festival/activities/LoginActivity.kt
@@ -9,46 +9,14 @@ import com.google.firebase.auth.FirebaseAuth
 import hu.bitclub.bce.festival.R
 
 class LoginActivity : AppCompatActivity() {
-    // See: https://developer.android.com/training/basics/intents/result
-    private val signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract()
-    ) { res ->
-        this.onSignInResult(res)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        startLoginUI()
     }
 
-    fun startLoginUI() {
-        // Choose authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-    }
-
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-        }
+    fun login(email: String, password: String) {
+        FirebaseAuth.getInstance()
+            .signInWithEmailAndPassword(email, password)
     }
 }
