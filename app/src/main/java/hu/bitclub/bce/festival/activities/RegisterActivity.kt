e026fcd826caee8f2e3099804f70938c9bba00b2
leventeve1 <selleylevente@gmail.com>
2021-12-04 18:34:57 +0100

    Register probalkozas

--- app/src/main/java/hu/bitclub/bce/festival/activities/RegisterActivity.kt
+++ app/src/main/java/hu/bitclub/bce/festival/activities/RegisterActivity.kt
@@ -2,6 +2,8 @@ package hu.bitclub.bce.festival.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import hu.bitclub.bce.festival.R
@@ -21,5 +23,16 @@ class RegisterActivity : AppCompatActivity() {
         FirebaseAuth.getInstance()
             .createUserWithEmailAndPassword(email, password)
             .addOnSuccessListener { Toast.makeText(this, "register sikeres", Toast.LENGTH_SHORT).show() }
+    }
+
+    fun login_click(view: View) {
+        var pass1 = findViewById<EditText>(R.id.password1EditText)
+        var pass2 = findViewById<EditText>(R.id.password2EditText)
+        var email = findViewById<EditText>(R.id.emailEditText)
+        if (pass1.text.toString() == pass2.text.toString()) {
+        register(email.text.toString(),pass1.text.toString())
+        }
+        else {
+            Toast.makeText( this,"nem sikerult", Toast.LENGTH_SHORT).show()}
     }
 }
